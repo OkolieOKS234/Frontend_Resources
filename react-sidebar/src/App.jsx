28bcfec8a47a795e32aa9ee206ab4d071edfd576
OkolieOKS234 <dokolie040@gmail.com>
2023-07-20 15:29:55 +0100

    added two folders

---
+++ react-sidebar/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+
+const App = () => {
+  return (
+    <div className="w-full h-screen bg-back object-cover flex items-center">
+      <Sidebar />
+    </div>
+  );
+};
+
+export default App;
