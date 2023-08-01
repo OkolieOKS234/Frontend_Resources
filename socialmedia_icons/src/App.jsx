3ebd476c3a726f7b0491e9984d419397dd6ad5bb
OkolieOKS234 <dokolie040@gmail.com>
2023-08-01 08:16:11 +0100

    added icons

---
+++ socialmedia_icons/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Colored_icons from './Colored_icons'
+import Black_and_white_icons from './black_and_white_icons'
+
+function App() {
+
+
+  return (
+    <>
+     <Black_and_white_icons/>
+     <Colored_icons/>
+    </>
+  )
+}
+
+export default App
